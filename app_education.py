ecbc296159a093fb2f80eb02924f647267fec58e
milehc <mhenaoci@cesde.net>
2024-10-23 21:24:58 -0500

    Commit

---
+++ app_education.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+#df = pd.read_csv('education.csv')
+
+st.title("Análisis de datos de educación en Colombia")
+
+uploaded_file = st.file_uploader("Cargar archivo 'education.csv'", type= ["csv"])
+if uploaded_file is not None:
+    df= pd.read_csv(uploaded_file)
+
+st.dataframe(df)
+
+st.sidebar.header("Filtros")
+nivel_educativo = st.sidebar.multiselect(
+"Nivel educativo", df["Nivel educativo"].unique()
+ )
+carrera = st.sidebar.multiselect("Carrera", df["Carrera"].unique())
+institucion = st.sidebar.multiselect("Institución", df["Institución"].unique())
+
+df_filtrado = df.copy()
+if nivel_educativo:
+ df_filtrado = df_filtrado[df_filtrado["Nivel educativo"].isin(nivel_educativo)]
+if carrera:
+ df_filtrado = df_filtrado[df_filtrado["Carrera"].isin(carrera)]
+if institucion:
+ df_filtrado = df_filtrado[df_filtrado["Institución"].isin(institucion)]
+
+st.dataframe(df_filtrado)
+
+#Calcular y mostrar estadísticas descriptivas de los datos filtrados
+
+st.subheader("Estadísticas Descriptivas")
+st.write(df_filtrado.describe())
+
+# Conteo de estudiantes por nivel educativo
+st.subheader("Conteo de estudiantes por nivel esducativo")
+st.bar_chart(df_filtrado["Nivel educativo"].value_counts())
+
+# Visualizar la distribución de la edad con un histograma
+st.subheader("Distribución de la Edad")
+
+fig, ax = plt.subplots()
+ax.hist(df_filtrado["Edad"], bins= 10)
+
+st.pyplot(fig)
